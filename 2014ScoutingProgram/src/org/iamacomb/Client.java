6321dddcbbec88ce262fa47399b62caed92a7d51
Alex Phan <alexp7361@gmail.com>
2015-07-09 10:26:40 -0400

    Added utility function that
    asks string and uses the java.util.Scanner to return string

--- 2014ScoutingProgram/src/org/iamacomb/Client.java
+++ 2014ScoutingProgram/src/org/iamacomb/Client.java
@@ -1,5 +1,22 @@
 package org.iamacomb;
 
-public class Client {
+import java.util.Scanner; //This will be the temporary input method in the release, AWT and Swing will be used later for a GUI
 
+public class Client {
+	public static void main(String[] args){
+		Client c = new Client();
+		String a = c.receive_input("How many boxes did they stack?");
+		System.out.println("Team 200 stacked " + a + " boxes");
+	}
+	
+	public String receive_input(String prompt){
+		String input;
+		Scanner input_ = new Scanner(System.in);
+		
+		System.out.print(prompt);
+		input = input_.next();
+		
+		
+		return input;
+	}
 }
